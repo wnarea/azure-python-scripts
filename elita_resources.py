afd51a5ac1197be8c841a4e7c3972efe5b29c03b
wnarea <wnarea@hotmail.com>
2025-08-29 18:50:39 +0000

    elita resources

---
+++ elita_resources.py
@@ -0,0 +1,383 @@
+elita_apim_test = [
+  {
+    "name": "apim-elita-test-us",
+    "resourceGroup": "RGElitaDev",
+    "location": "northcentralus",
+    "subscriptionId": "71d00b98-02b3-47df-961f-b2516cbb33ca",
+    "subscriptionName": "Assurant Spoke NorthCentralUS SharedNonProd",
+    "costCenter": 8560461,
+    "region": "us",    
+    "elita": {
+      "resourceGroup": "RGElitaDev",
+      "subscriptionId": "71d00b98-02b3-47df-961f-b2516cbb33ca",
+      "subscriptionName": "Assurant Spoke NorthCentralUS SharedNonProd",
+    }
+  },
+  {
+    "name": "apim-elita-test-eu",
+    "resourceGroup": "RGElitaDev",
+    "location": "uksouth",
+    "subscriptionId": "c4ef5634-9b63-482c-9c5a-8a19aa8b47a6",
+    "subscriptionName": "Assurant Spoke UKSouth SharedNonProd",
+    "costCenter": 8560461,
+    "region": "eu",    
+    "elita": {
+      "resourceGroup": "RGElitaDev",
+      "subscriptionId": "c4ef5634-9b63-482c-9c5a-8a19aa8b47a6",
+      "subscriptionName": "Assurant Spoke UKSouth SharedNonProd",
+    }
+  },
+  {
+    "name": "apim-elita-test-ap",
+    "resourceGroup": "RGElitaDev",
+    "location": "australiasoutheast",
+    "subscriptionId": "0a511cec-cc18-4b13-83a3-e1460006498e",
+    "subscriptionName": "Assurant Spoke AustraliaSouthEast SharedNonProd",
+    "costCenter": 8560461,
+    "region": "ap",    
+    "elita": {
+      "resourceGroup": "RGElitaDev",
+      "subscriptionId": "0a511cec-cc18-4b13-83a3-e1460006498e",
+      "subscriptionName": "Assurant Spoke AustraliaSouthEast SharedNonProd",
+    }
+  },
+  {
+    "name": "apim-elita-test-jp",
+    "resourceGroup": "RGElitaDev",
+    "location": "japaneast",
+    "subscriptionId": "f55d53e8-59f2-4286-ba43-85d4d3b78785",
+    "subscriptionName": "Assurant Spoke JapanEast SharedNonProd",
+    "costCenter": 8560461,
+    "region": "jp",    
+    "elita": {
+      "resourceGroup": "RGElitaDev",
+      "subscriptionId": "f55d53e8-59f2-4286-ba43-85d4d3b78785",
+      "subscriptionName": "Assurant Spoke JapanEast SharedNonProd",
+    }
+  },
+  {
+    "name": "apim-elita-test-sa",
+    "resourceGroup": "RGElitaDev",
+    "location": "northcentralus",
+    "subscriptionId": "71d00b98-02b3-47df-961f-b2516cbb33ca",
+    "subscriptionName": "Assurant Spoke NorthCentralUS SharedNonProd",
+    "costCenter": 8560461,
+    "region": "sa",    
+    "elita": {
+      "resourceGroup": "RGElitaDev",
+      "subscriptionId": "71d00b98-02b3-47df-961f-b2516cbb33ca",
+      "subscriptionName": "Assurant Spoke NorthCentralUS SharedNonProd",
+    }
+  },
+  {
+    "name": "apim-elita-test-de",
+    "resourceGroup": "RGElitaDev",
+    "location": "germanywestcentral",
+    "subscriptionId": "eff2b514-ac21-4c92-840c-c00008fa602d",
+    "subscriptionName": "Assurant Spoke Germany West Central SharedNonProd",
+    "costCenter": 8560461,
+    "region": "de",    
+    "elita": {
+      "resourceGroup": "RGElitaDev",
+      "subscriptionId": "eff2b514-ac21-4c92-840c-c00008fa602d",
+      "subscriptionName": "Assurant Spoke Germany West Central SharedNonProd",
+    }
+  }
+]  
+
+elita_apim_prod = [
+  {
+    "name": "apim-uk-prod01",
+    "resourceGroup": "rgapim-prod",
+    "location": "uksouth",
+    "subscriptionId": "7fd054b4-b369-40db-bb60-58cceaf53488",
+    "subscriptionName": "Assurant Spoke UKSouth SharedProd",
+    "costCenter": 8560461,
+    "region": "eu"
+  },
+  {
+    "name": "apim-jp-prod01",
+    "resourceGroup": "rgapim-prod",
+    "location": "japaneast",
+    "subscriptionId": "36ad9702-b144-4166-9757-49642452cab2",
+    "subscriptionName": "Assurant Spoke JapanEast SharedProd",
+    "costCenter": 8560461,
+    "region": "jp"
+  },
+  {
+    "name": "apim-de-prod",
+    "resourceGroup": "rgapim-prod",
+    "location": "germanywestcentral",
+    "subscriptionId": "4841df6f-3e0b-4027-9389-f5c56600a2e3",
+    "subscriptionName": "Assurant Spoke Germany West Central SharedProd",
+    "costCenter": 8560461,
+    "region": "de"
+  },
+  {
+    "name": "apim-prod-us",
+    "resourceGroup": "rgapim-prod",
+    "location": "centralus",
+    "subscriptionId": "f7352245-06c3-4dc5-93dc-8b4fdba6b991",
+    "subscriptionName": "Assurant Spoke CentralUS SharedProd",
+    "costCenter": 8565060,
+    "region": "us"
+  },
+  {
+    "name": "apim-au-prod01",
+    "resourceGroup": "rgapim-prod",
+    "location": "australiasoutheast",
+    "subscriptionId": "fee7fbde-5414-4b10-a225-c25fbed93cc9",
+    "subscriptionName": "Assurant Spoke AustraliaSouthEast SharedProd",
+    "costCenter": 8560461,
+    "region": "ap"
+  },
+]
+
+elita_apim_prod_dr = [
+  {
+    "name": "apim-uk-prod",
+    "resourceGroup": "rgapim-prod",
+    "location": "ukwest",
+    "subscriptionId": "1b730a1e-a5f0-416b-9508-f6c5d890cb16",
+    "subscriptionName": "Assurant Spoke UKWest SharedProd",
+    "costCenter": 8560461,
+    "region": "eu"
+  },
+  {
+    "name": "apim-de-auth-prod02",
+    "resourceGroup": "rgapim-prod",
+    "location": "ukwest",
+    "subscriptionId": "1b730a1e-a5f0-416b-9508-f6c5d890cb16",
+    "subscriptionName": "Assurant Spoke UKWest SharedProd",
+    "costCenter": 8560461,
+    "region": "de"
+  },
+  {
+    "name": "apim-uk-prod02",
+    "resourceGroup": "rgapim-prod",
+    "location": "ukwest",
+    "subscriptionId": "1b730a1e-a5f0-416b-9508-f6c5d890cb16",
+    "subscriptionName": "Assurant Spoke UKWest SharedProd",
+    "costCenter": 8560461,
+    "region": "eu"
+  },
+  {
+    "name": "apim-uk-prod01",
+    "resourceGroup": "rgapim-prod",
+    "location": "uksouth",
+    "subscriptionId": "7fd054b4-b369-40db-bb60-58cceaf53488",
+    "subscriptionName": "Assurant Spoke UKSouth SharedProd",
+    "costCenter": 8560461,
+    "region": "eu"
+  },
+  {
+    "name": "apim-jp-prod02",
+    "resourceGroup": "rgapim-prod",
+    "location": "japanwest",
+    "subscriptionId": "07349c3c-6350-4d3b-b4a9-d84dbf009d05",
+    "subscriptionName": "Assurant Spoke JapanWest SharedProd",
+    "costCenter": 8560461,
+    "region": "jp"
+  },
+  {
+    "name": "apim-jp-prod01",
+    "resourceGroup": "rgapim-prod",
+    "location": "japaneast",
+    "subscriptionId": "36ad9702-b144-4166-9757-49642452cab2",
+    "subscriptionName": "Assurant Spoke JapanEast SharedProd",
+    "costCenter": 8560461,
+    "region": "jp"
+  },
+  {
+    "name": "apim-de-auth-prod01",
+    "resourceGroup": "rgapim-prod",
+    "location": "germanywestcentral",
+    "subscriptionId": "4841df6f-3e0b-4027-9389-f5c56600a2e3",
+    "subscriptionName": "Assurant Spoke Germany West Central SharedProd",
+    "costCenter": 8560461,
+    "region": "de"
+  },
+  {
+    "name": "apim-de-prod",
+    "resourceGroup": "rgapim-prod",
+    "location": "germanywestcentral",
+    "subscriptionId": "4841df6f-3e0b-4027-9389-f5c56600a2e3",
+    "subscriptionName": "Assurant Spoke Germany West Central SharedProd",
+    "costCenter": 8560461,
+    "region": "de"
+  },
+  {
+    "name": "apim-prod-us",
+    "resourceGroup": "rgapim-prod",
+    "location": "centralus",
+    "subscriptionId": "f7352245-06c3-4dc5-93dc-8b4fdba6b991",
+    "subscriptionName": "Assurant Spoke CentralUS SharedProd",
+    "costCenter": 8565060,
+    "region": "us"
+  },
+  {
+    "name": "apim-au-prod01",
+    "resourceGroup": "rgapim-prod",
+    "location": "australiasoutheast",
+    "subscriptionId": "fee7fbde-5414-4b10-a225-c25fbed93cc9",
+    "subscriptionName": "Assurant Spoke AustraliaSouthEast SharedProd",
+    "costCenter": 8560461,
+    "region": "ap"
+  },
+  {
+    "name": "apim-au-auth-prod01",
+    "resourceGroup": "rgapim-prod",
+    "location": "australiasoutheast",
+    "subscriptionId": "fee7fbde-5414-4b10-a225-c25fbed93cc9",
+    "subscriptionName": "Assurant Spoke AustraliaSouthEast SharedProd",
+    "costCenter": 8560461,
+    "region": "ap"
+  },
+  {
+    "name": "apim-au-prod02",
+    "resourceGroup": "rgapim-prod",
+    "location": "australiaeast",
+    "subscriptionId": "59e584f7-c9aa-450c-8e89-45ab1741e922",
+    "subscriptionName": "Assurant Spoke AustraliaEast SharedProd",
+    "costCenter": 8560461,
+    "region": "ap"
+  },
+  {
+    "name": "apim-au-auth-prod02",
+    "resourceGroup": "rgapim-prod",
+    "location": "australiaeast",
+    "subscriptionId": "59e584f7-c9aa-450c-8e89-45ab1741e922",
+    "subscriptionName": "Assurant Spoke AustraliaEast SharedProd",
+    "costCenter": 8560461,
+    "region": "ap"
+  }
+]
+
+elita_apim_shared_modl = [
+  {
+    "ElitaRegion": "Australia",
+    "ApimServiceName": "apim-au-model01",
+    "GatewayURL": "https://apim-model.au.assurant.com",
+    "ResourceGroup": "RGAPIM-Model",
+    "Subscription": "Assurant Spoke AustraliaSouthEast SharedProd",
+    "SubscriptionId": "fee7fbde-5414-4b10-a225-c25fbed93cc9",
+    "AzureLocation": "Australia Southeast",
+    "SubscriptionIdentifier": "elita-p2-system"
+  },
+  {
+    "ElitaRegion": "UK",
+    "ApimServiceName": "apim-uk-model01",
+    "GatewayURL": "https://apim-model.uk.assurant.com",
+    "ResourceGroup": "RGAPIM-Model",
+    "Subscription": "Assurant Spoke UKSouth SharedProd",
+    "SubscriptionId": "7fd054b4-b369-40db-bb60-58cceaf53488",
+    "AzureLocation": "UK South",
+    "SubscriptionIdentifier": "elita-e2-system"
+  },
+  {
+    "ElitaRegion": "Germany",
+    "ApimServiceName": "apim-de-model",
+    "GatewayURL": "https://apim-model.de.assurant.com",
+    "ResourceGroup": "RGAPIM-Model",
+    "Subscription": "Assurant Spoke Germany West Central SharedProd",
+    "SubscriptionId": "4841df6f-3e0b-4027-9389-f5c56600a2e3",
+    "AzureLocation": "Germany West Central",
+    "SubscriptionIdentifier": "elita-d2-system"
+  },
+  {
+    "ElitaRegion": "US",
+    "ApimServiceName": "api-model",
+    "GatewayURL": "https://api-model.assurant.com",
+    "ResourceGroup": "rgapim-model",
+    "Subscription": "Assurant Spoke CentralUS SharedProd",
+    "SubscriptionId": "f7352245-06c3-4dc5-93dc-8b4fdba6b991",
+    "AzureLocation": "Central US",
+    "SubscriptionIdentifier": "elita-u2-system"
+  },
+  {
+    "ElitaRegion": "US",
+    "ApimServiceName": "api-model",
+    "GatewayURL": "https://api-model.assurant.com",
+    "ResourceGroup": "rgapim-model",
+    "Subscription": "Assurant Spoke CentralUS SharedProd",
+    "SubscriptionId": "f7352245-06c3-4dc5-93dc-8b4fdba6b991",
+    "AzureLocation": "Central US",
+    "SubscriptionIdentifier": "elita-u3-system"
+  },
+  {
+    "ElitaRegion": "Latam",
+    "ApimServiceName": "api-model",
+    "GatewayURL": "https://api-model.assurant.com",
+    "ResourceGroup": "rgapim-model",
+    "Subscription": "Assurant Spoke CentralUS SharedProd",
+    "SubscriptionId": "f7352245-06c3-4dc5-93dc-8b4fdba6b991",
+    "AzureLocation": "Central US",
+    "SubscriptionIdentifier": "elita-a2-system"
+  },
+  {
+    "ElitaRegion": "Japan",
+    "ApimServiceName": "apim-jp-model01",
+    "GatewayURL": "https://apim-model.jp.assurant.com",
+    "ResourceGroup": "RGAPIM-Model",
+    "Subscription": "Assurant Spoke JapanEast SharedProd",
+    "SubscriptionId": "36ad9702-b144-4166-9757-49642452cab2",
+    "AzureLocation": "Japan East",
+    "SubscriptionIdentifier": "elita-j2-system"
+  }
+]
+
+elita_apim_shared_jp_modl = [
+  {
+    "ElitaRegion": "Japan",
+    "ApimServiceName": "apim-jp-model01",
+    "GatewayURL": "https://apim-model.jp.assurant.com",
+    "ResourceGroup": "RGAPIM-Model",
+    "Subscription": "Assurant Spoke JapanEast SharedProd",
+    "SubscriptionId": "36ad9702-b144-4166-9757-49642452cab2",
+    "AzureLocation": "Japan East",
+    "SubscriptionIdentifier": "elita-j1-statuscheck-v1"
+  },
+  {
+    "ElitaRegion": "Japan",
+    "ApimServiceName": "apim-jp-model01",
+    "GatewayURL": "https://apim-model.jp.assurant.com",
+    "ResourceGroup": "RGAPIM-Model",
+    "Subscription": "Assurant Spoke JapanEast SharedProd",
+    "SubscriptionId": "36ad9702-b144-4166-9757-49642452cab2",
+    "AzureLocation": "Japan East",
+    "SubscriptionIdentifier": "elita-j2-statuscheck-v1"
+  },
+  {
+    "ElitaRegion": "Japan",
+    "ApimServiceName": "apim-jp-model01",
+    "GatewayURL": "https://apim-model.jp.assurant.com",
+    "ResourceGroup": "RGAPIM-Model",
+    "Subscription": "Assurant Spoke JapanEast SharedProd",
+    "SubscriptionId": "36ad9702-b144-4166-9757-49642452cab2",
+    "AzureLocation": "Japan East",
+    "SubscriptionIdentifier": "elita-j3-statuscheck-v1"
+  }
+]
+
+elita_apim_test_us = [
+  {
+    "ElitaRegion": "US",
+    "ApimServiceName": "apim-elita-test-us",
+    "GatewayURL": "https://apim-elita-test-us.azure-api.net",
+    "ResourceGroup": "RGElitaDev",
+    "Subscription": "Assurant Spoke NorthCentralUS SharedNonProd",
+    "SubscriptionId": "71d00b98-02b3-47df-961f-b2516cbb33ca",
+    "AzureLocation": "North Central US",
+    "SubscriptionIdentifier": "elita-u2-summitracing-v1"
+  },
+  {
+    "ElitaRegion": "US",
+    "ApimServiceName": "apim-elita-test-us",
+    "GatewayURL": "https://apim-elita-test-us.azure-api.net",
+    "ResourceGroup": "RGElitaDev",
+    "Subscription": "Assurant Spoke NorthCentralUS SharedNonProd",
+    "SubscriptionId": "71d00b98-02b3-47df-961f-b2516cbb33ca",
+    "AzureLocation": "North Central US",
+    "SubscriptionIdentifier": "Elita-u2-BestBuy"
+  }
+]
